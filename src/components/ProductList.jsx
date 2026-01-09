29074d0b1223e1bc5b65323d9edb73d468acb757
Samm005 <samudyathakaranth@gmail.com>
2026-01-10 00:03:44 +0530

    Created utils folder with useFetch.js inside it defining the custom hook to fetch data from an API

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,12 @@
+import { useEffect, useState } from "react";
+import "./ProductList.css";
+
 function ProductList() {
-  return <></>;
+  return (
+    <div className="product-list">
+      <h2 id="Heading">ShoppyGlobe</h2>
+    </div>
+  );
 }
 
 export default ProductList;
---
+++ src/utils/useFetch.js
@@ -0,0 +1,33 @@
+//A custom hook to fetch data from the API
+
+import { useState, useEffect } from "react";
+
+function useFetch(URL) {
+  const [data, setData] = useState([]); // Set state for data from API
+  const [loading, setLoading] = useState(true); // Set loading status of data
+  const [error, setError] = useState(null); // Set error if API fetch throws error
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status:${response.status}`);
+        }
+
+        const result = await response.json();
+        setData(result.products);
+      } catch (err) {
+        setError(`Failed to fetch the products ${err}`);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [URL]); // In case of URL change, data is refetched
+
+  return { data, loading, error };
+}
+
+export default useFetch;
