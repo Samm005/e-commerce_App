0feadb4691ecc7f2c21159f2ba5dcb1f34304caa
Samm005 <samudyathakaranth@gmail.com>
2026-01-10 19:57:37 +0530

    Cart component was defined and coded

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,44 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartItem from "./CartItem";
+
 function Cart() {
-  return <></>;
+  const cartItems = useSelector((state) => state.cart.items);
+
+  const totalAmount = cartItems.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+
+  if (cartItems.length === 0) {
+    return (
+      <div className="cart-container">
+        <h2>Your cart is empty</h2>
+        <Link to="/" className="home-btn">
+          Go back to shopping
+        </Link>
+      </div>
+    );
+  }
+
+  return (
+    <div className="cart-container">
+      <h2>Your Cart</h2>
+
+      <div className="cart-items">
+        {cartItems.map((item) => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
+
+      <div className="cart-summary">
+        <h3>Total: ${totalAmount.toFixed(2)}</h3>
+        <Link to="/checkout" className="checkout-btn">
+          Proceed to Checkout
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Cart;
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addToCart } from "../utils/cartSlice";
+import "./ProductItem.css";
 
 function ProductItem({ product }) {
   const dispatch = useDispatch();
