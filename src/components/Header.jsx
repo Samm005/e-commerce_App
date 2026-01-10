298ebc83f5210aa5c3d1910623ca5e8bd1992114
Samm005 <samudyathakaranth@gmail.com>
2026-01-10 16:33:24 +0530

    Added navigation/header functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import "./Header.css";
+
 function Header() {
-  return <></>;
+  const cartItems = useSelector((state) => state.cart.items);
+
+  const totalItems = cartItems.reduce(
+    (total, item) => total + item.quantity,
+    0
+  );
+
+  return (
+    <header className="header">
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/cart" className="cart-link">
+          🛒 ({totalItems})
+        </Link>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
