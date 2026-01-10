05f19c861833909bf13488617f75c4a41a4b2436
Samm005 <samudyathakaranth@gmail.com>
2026-01-11 00:50:25 +0530

    Optimized performance using lazy and suspense

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import ProductList from "./components/ProductList.jsx";
-import ProductDetail from "./components/ProductDetail.jsx";
-import Cart from "./components/Cart.jsx";
-import Checkout from "./components/Checkout.jsx";
-import NotFound from "./components/NotFound.jsx";
 import { store } from "./utils/store";
+import { lazy, Suspense } from "react";
+
+const ProductList = lazy(() => import("./components/ProductList.jsx"));
+const ProductDetail = lazy(() => import("./components/ProductDetail.jsx"));
+const Cart = lazy(() => import("./components/Cart.jsx"));
+const Checkout = lazy(() => import("./components/Checkout.jsx"));
+const NotFound = lazy(() => import("./components/NotFound.jsx"));
 
 const approutes = createBrowserRouter([
   {
@@ -40,7 +42,25 @@ const approutes = createBrowserRouter([
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={approutes} />
+      <Suspense
+        fallback={
+          <h2
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              margin: 0,
+              color: "#bbf900",
+              background:
+                "radial-gradient(circle at top, #2b1055 0%, #1a1a2e 50%, #000000 100%)",
+            }}
+          >
+            Please wait! Loading....
+          </h2>
+        }
+      >
+        <RouterProvider router={approutes} />
+      </Suspense>
     </Provider>
   </StrictMode>
 );
