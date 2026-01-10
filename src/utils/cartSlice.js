6531ebcfceee8e5a5fa4b9f8f82bfa660773efcb
Samm005 <samudyathakaranth@gmail.com>
2026-01-10 13:54:29 +0530

    Created files for redux and added code functionality for cart

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart(state, action) {
+      const item = state.items.find((i) => i.id === action.payload.id);
+
+      if (item) {
+        item.quantity += 1; //If the item already exists in card, increase the quantity
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+
+    removeFromCart(state, action) {
+      state.items = state.items.filter((item) => item.id !== action.payload); //remove item from the cart
+    },
+
+    incrementQuantity(state, action) {
+      const item = state.items.find((i) => i.id === action.payload); // increment the quantity in cart
+      if (item) item.quantity += 1;
+    },
+
+    decrementQuantity(state, action) {
+      const item = state.items.find((i) => i.id === action.payload); // decrement the quantity in cart
+      if (item && item.quantity > 1) {
+        item.quantity -= 1;
+      }
+    },
+  },
+});
+
+export const {
+  addToCart,
+  removeFromCart,
+  incrementQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
+
+export const selectCartItems = (state) => state.cart.items;
+
+export default cartSlice.reducer;
