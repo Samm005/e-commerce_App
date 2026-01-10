f7c81457af7955577dd7dfb2fc3ffe195842d3bb
Samm005 <samudyathakaranth@gmail.com>
2026-01-10 14:26:32 +0530

    store.js coded

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 import ProductList from "./components/ProductList.jsx";
 import ProductDetail from "./components/ProductDetail.jsx";
 import Cart from "./components/Cart.jsx";
 import Checkout from "./components/Checkout.jsx";
 import NotFound from "./components/NotFound.jsx";
+import { store } from "./redux/store";
 
 const approutes = createBrowserRouter([
   {
@@ -37,6 +39,8 @@ const approutes = createBrowserRouter([
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={approutes} />
+    <Provider store={store}>
+      <RouterProvider router={approutes} />
+    </Provider>
   </StrictMode>
 );
