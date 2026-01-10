690b2223698c2115517a434c629c8271255843f9
Samm005 <samudyathakaranth@gmail.com>
2026-01-10 23:17:09 +0530

    NotFound page has been added

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,5 +1,22 @@
+import { useRouteError } from "react-router-dom";
+import "./errorPage.css";
+import { Link } from "react-router-dom";
+
 function NotFound() {
-  return <></>;
+  const err = useRouteError();
+  console.log(err);
+  return (
+    <div className="err-data">
+      <p id="error">{err.data}</p>
+      <p id="status">
+        {err.status} {err.statusText}
+      </p>
+      <h2 id="final">Please enter the proper destination...</h2>
+      <Link to="/" id="home-link">
+        Home
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
